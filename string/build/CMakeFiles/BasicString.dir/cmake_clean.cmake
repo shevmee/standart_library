3fa0053e9319c272f5b90facfe8443cd9335f3f0
mshevtso <mshevtso@cisco.com>
2024-12-09 15:06:08 -0500

    improv: add constructors, functions and basic UTs

---
+++ string/build/CMakeFiles/BasicString.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "BasicString"
+  "BasicString.pdb"
+  "CMakeFiles/BasicString.dir/main.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/BasicString.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ string/build/third_party/googletest/googlemock/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/admin/cpp_trainings/standart_library/string/third_party/googletest/googlemock
+# Build directory: /home/admin/cpp_trainings/standart_library/string/build/third_party/googletest/googlemock
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("../googletest")
