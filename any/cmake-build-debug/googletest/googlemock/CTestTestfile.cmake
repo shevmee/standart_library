c8a4c3423004f3176b999e2336228fdee99a434b
mshevtso <mshevtso@cisco.com>
2024-12-12 14:32:04 -0500

    improv: deleted redundant dirs

--- any/cmake-build-debug/googletest/googlemock/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/shevmee/CLionProjects/googletest/googlemock
-# Build directory: /home/shevmee/CLionProjects/standart_library/any/cmake-build-debug/googletest/googlemock
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("../googletest")
